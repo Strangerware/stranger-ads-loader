fc9b89d23012d47487aebf3c346fb849c29d18ed
fetz <fetzpt@gmail.com>
2016-10-01 12:20:20 +0100

    refactor: reorganise files

--- test/adsloader/fetchAd.spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import test from 'ava';
-import fetchAd from '../../src/adsLoader/fetchAd';
-
-test(`must reject the promise if you don't pass a masterAdTag`,
-  t => t.throws(fetchAd({}), Error, 'fetchAd missing masterAdTag'));
-
-
