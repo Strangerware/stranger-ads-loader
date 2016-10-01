68a99fe540807556e405a4aa308ec4822740241b
fetz <fetzpt@gmail.com>
2016-10-01 16:04:51 +0100

    feat: add fetchAd method
    To fetch the VAST XML and parse it to object

---
+++ test/adsLoader.spec.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import adsLoader from '../src/adsLoader';
+
+test(`must reject the promise if you don't pass a masterAdTag`, t => t.throws(
+  adsLoader(), 'adsLoader missing masterAdTag'
+));
+
+// test(`must return a pri`)
