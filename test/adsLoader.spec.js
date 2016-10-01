c2644bd9839f171e30e5d76d89b6612e1b7e00a1
Carlos Serrano <carlos.pastor@mailonline.co.uk>
2016-10-01 16:48:03 +0100

    stile: remove commented code

--- test/adsLoader.spec.js
+++ test/adsLoader.spec.js
@@ -4,5 +4,3 @@ import adsLoader from '../src/adsLoader';
 test(`must reject the promise if you don't pass a masterAdTag`, t => t.throws(
   adsLoader(), 'adsLoader missing masterAdTag'
 ));
-
-// test(`must return a pri`)
